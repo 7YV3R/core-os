1d448478628c0d51d59508c2b56ffb6fecc44d93
7yv3r <dominik.tieber@titan-sec.com>
2025-11-03 22:02:32 +0100

    added comment for description

--- build/10_install_kde.sh
+++ build/10_install_kde.sh
@@ -5,6 +5,7 @@ set -euox pipefail
 # install KDE
 dnf5 group install -y kde-desktop
 
+# setup sysusersd for abrt user
 cat <<EOF > "/usr/lib/sysusers.d/abrt.conf"
 #Type	Name	ID			    GECOS				Home directory Shell
 u!      abrt    173             "Automatic Bug Reporting Tool (ABRT)"   /usr/sbin/nologin
