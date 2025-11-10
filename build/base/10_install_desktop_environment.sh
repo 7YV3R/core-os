f9d3d4f331eca27ebeed5654b3c0afa86430593c
7yv3r <dominik.tieber@titan-sec.com>
2025-11-10 23:47:19 +0100

    removed false information

--- build/base/10_install_desktop_environment.sh
+++ build/base/10_install_desktop_environment.sh
@@ -6,14 +6,12 @@ set -euox pipefail
 # This parent script installs desktop environments
 # Disable or enable other desktop environments via
 # (un)commenting of the build script.
-# Attention! the last installed desktop environment will
-# be set as default.
 
 # Install KDE
 /build/11_install_kde.sh
 
 # Install Hyprland
-#/build/12_install_hyprland.sh
+/build/12_install_hyprland.sh
 
 # start directly into installed desktop environment
 systemctl set-default graphical.target
